220f01e43c840a08f3b51cc30012bdcdcde3f3c8
Meital Weiss <meital.weiss@lemonade.com>
2020-12-14 10:03:33 +0200

    remove unused session controllet

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
       resources :users 
       post '/sign_in' => 'users#sign_in'
       post '/sign_out' => 'users#sign_out'
-      # resources :sessions, only: [:new, :create, :destroy]
     end
   end
 end
